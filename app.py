41ae86844f13d5a2ab499db4dc6b5ac2989d85cc
AhmedSalam22 <Ahmed0salam22@gmail.com>
2020-07-24 09:14:43 +0200

    Hypothesis Testing 4-6

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ hypothesis_testing = st.sidebar.checkbox("Hypothesis Testing" , False)
 
 if hypothesis_testing:
     st.title("Hypothesis Testing")
-    options = st.sidebar.radio("Select option" , ["Option {x}".format(x=x) for x in range(1,4)])
+    options = st.sidebar.radio("Select option" , ["Option {x}".format(x=x) for x in range(1,7)])
 
     x = st.number_input("X value" , 0.0)
 
@@ -52,57 +52,84 @@ if hypothesis_testing:
                 $$H_1: \mu > {x}$$
 
                 **option4**
-                        
+                $$H_0: \mu_e <= \mu_c$$
+                $$H_1: \mu_e > \mu_c$$    
 
+                **option5**
+                $$H_0: \mu_e >= \mu_c$$
+                $$H_1: \mu_e < \mu_c$$   
+
+                **option6**
+                $$H_0: \mu_e - \mu_c = 0$$ 
+                $$H_1: \mu_e - \mu_c != 0$$   
 
             """.format(x=x))
     if options in ["Option 1" , "Option 2" , "Option 3"] and file_uploader != None:
         controler = st.selectbox("Select the Controler group" , list(df.columns))
+    elif options in ["Option 4" , "Option 5" , "Option 6"] and file_uploader != None:
+        controler = st.selectbox("Select the Controler group" , list(df.columns))
+        experiment = st.selectbox("Select the experiment group" , list(df.columns))
     if st.button("Calculate"):
 
         sampling_dist = []
         my_bar = st.progress(0)
+        p = st.empty()
+        with st.echo():
+            if options in ["Option 1" , "Option 2" , "Option 3"]:
+                #Generate Normal distrubution from bootstraping 
+                for i in range(10000):
+                    p.text ("In progress... {:.2f} %".format(i / 10000 * 100))
+                    sample = df.sample(df.shape[0], replace = True)
+                    sample_mean = sample[controler].mean()
+                    sampling_dist.append(sample_mean)
+                    my_bar.progress(i/10000)
+            elif options in ["Option 4" , "Option 5" , "Option 6"]:
+                    for i in range(10000):
+                        p.text ("In progress... {:.2f} %".format(i / 10000 * 100))
+                        sample = df.sample(df.shape[0], replace = True)
+                        sample_mean1 = sample[controler].mean()
+                        sample_mean2 = sample[experiment].mean()
+
+                        sampling_dist.append(sample_mean2 -  sample_mean1)
+                        my_bar.progress(i/10000)
         
-        with st.spinner(text='In progress...'):
-            for i in range(10000):
-                sample = df.sample(df.shape[0], replace = True)
-                sample_mean = sample[controler].mean()
-                sampling_dist.append(sample_mean)
-                my_bar.progress(i/10000)
-        
-     
+    
+            # assume the null value is True
+            null_vals = np.random.normal(x, np.std(sampling_dist), 10000)
 
-        null_vals = np.random.normal(x, np.std(sampling_dist), 10000)
-        plt.figure(figsize=[15,10])
-        plt.subplot(1,2,1)
-        plt.hist(sampling_dist)
-        plt.title("sampling distribution from bootsstraping")
+            #visualize data
+            plt.figure(figsize=[15,10])
+            plt.subplot(1,2,1)
+            plt.hist(sampling_dist)
+            plt.title("sampling distribution from bootsstraping")
 
-        plt.subplot(1,2,2)
-        plt.hist(null_vals)
-        plt.title("if we assume the null value is True then the distibution looks like")
+            plt.subplot(1,2,2)
+            plt.hist(null_vals)
+            plt.title("if we assume the null value is True then the distibution looks like")
 
-        obs_mean = df[controler].mean()
+            obs_mean = df[controler].mean()
 
-        plt.axvline(x=obs_mean, color = 'red'); # where our sample mean falls on null dist
-        plt.axvline(x= x - (obs_mean - x), color = 'red'); # where our sample mean falls on null dist
+            plt.axvline(x=obs_mean, color = 'red'); # where our sample mean falls on null dist
+            plt.axvline(x= x - (obs_mean - x), color = 'red'); # where our sample mean falls on null dist
 
-        st.pyplot()
-        obs_mean = df[controler].mean()
+            st.pyplot()
 
-        if options == "Option 1":
-            # probability of a statistic higher than observed
-            prob_more_extreme_high = (null_vals > obs_mean).mean()             
-            # probability a statistic is more extreme lower
-            prob_more_extreme_low = (x - (obs_mean - x) < null_vals).mean()
-            pval = prob_more_extreme_low + prob_more_extreme_high
-            st.markdown("Pval =  `{}`".format(pval))
-        elif options == "Option 3":
-            pval = (null_vals > obs_mean).mean()
-            st.markdown("Pval =  `{}`".format(pval))
-        elif options == "Option 2":
-            pval = (null_vals < obs_mean).mean()
-            st.markdown("Pval =  `{}`".format(pval))
+        if options == "Option 1" or options == "Option 6":
+            with st.echo():
+                # probability of a statistic higher than observed
+                prob_more_extreme_high = (null_vals > obs_mean).mean()             
+                # probability a statistic is more extreme lower
+                prob_more_extreme_low = (x - (obs_mean - x) < null_vals).mean()
+                pval = prob_more_extreme_low + prob_more_extreme_high
+                st.markdown("Pval =  `{}`".format(pval))
+        elif options in ["Option 3" , "Option 4"]:
+            with st.echo():
+                pval = (null_vals > obs_mean).mean()
+                st.markdown("Pval =  `{}`".format(pval))
+        elif options == "Option 2" or options == "Option 5":
+            with st.echo():
+                pval = (null_vals < obs_mean).mean()
+                st.markdown("Pval =  `{}`".format(pval))
 # regression
 if st.sidebar.checkbox("regression" , False):
     linearOrLogistic = st.sidebar.radio("Type of regression" , ["linear regression","Logistic regression"])
